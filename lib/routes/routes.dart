1d9311c7270303d32bef4a3e6bd0c0ee1d784256
Aakashdeep Sil <aakashdeep.work@gmail.com>
2024-10-20 17:50:29 -0500

    old profile files deleted

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:laya/about_us.dart';
-import 'package:laya/complete_profile.dart';
-import 'package:laya/create_post.dart';
-import 'package:laya/explore/search.dart';
-import 'package:laya/home.dart';
+import 'package:laya/config/schema/profiles.dart';
+import 'package:laya/features/auth/presentation/complete_profile_page.dart';
+import 'package:laya/error.dart';
+import 'package:laya/features/auth/presentation/sign_in_page.dart';
+import 'package:laya/features/auth/presentation/sign_up_page.dart';
+import 'package:laya/features/home/presentation/home_page.dart';
+import 'package:laya/features/profile/presentation/edit_profile.dart';
+import 'package:laya/features/profile/presentation/profile_page.dart';
+import 'package:laya/features/profile/presentation/profile_settings_page.dart';
 import 'package:laya/landing.dart';
-import 'package:laya/posts/post_detail.dart';
-import 'package:laya/profile/edit_profile.dart';
-import 'package:laya/profile/profile.dart';
-import 'package:laya/profile/profile_settings.dart';
-import 'package:laya/sign_in.dart';
-import 'package:laya/profile/update_password.dart';
-import 'package:laya/comments/add_comment_page.dart';
-import 'package:laya/socials/socials_homepage.dart';
+import 'package:laya/splash.dart';
+
+final GoRouter router = GoRouter(
+  errorBuilder: (context, state) => ErrorScreen(error: state.error),
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) =>
+          const SplashPage(),
+      routes: routes,
+    ),
+  ],
+);
 
 List<RouteBase> routes = [
   GoRoute(
@@ -31,83 +41,95 @@ List<RouteBase> routes = [
   GoRoute(
     path: 'sign_in',
     builder: (BuildContext context, GoRouterState state) {
-      return const SignUp();
+      return const SignInPage();
     },
   ),
   GoRoute(
-    path: 'update_password',
+    path: 'sign_up',
     builder: (BuildContext context, GoRouterState state) {
-      return const UpdatePassword();
+      return const SignUpPage();
     },
   ),
+  // GoRoute(
+  //   path: 'update_password',
+  //   builder: (BuildContext context, GoRouterState state) {
+  //     return const UpdatePassword();
+  //   },
+  // ),
   GoRoute(
     path: 'complete_profile',
     builder: (BuildContext context, GoRouterState state) {
-      return const CompleteProfile();
+      final profile = state.extra as Profile;
+      return CompleteProfilePage(profile: profile);
     },
   ),
   GoRoute(
     path: 'home',
     builder: (BuildContext context, GoRouterState state) {
-      return const Home();
+      final profile = state.extra as Profile;
+      return HomePage(profile: profile);
     },
   ),
-  GoRoute(
-    path: 'socials',
-    builder: (BuildContext context, GoRouterState state) {
-      return const Socials();
-    },
-  ),
-  GoRoute(
-    path: 'post_details/:postID',
-    builder: (BuildContext context, GoRouterState state) {
-      final extraData = state.extra as Map<String, dynamic>?;
+  // GoRoute(
+  //   path: 'socials',
+  //   builder: (BuildContext context, GoRouterState state) {
+  //     return const Socials();
+  //   },
+  // ),
+  // GoRoute(
+  //   path: 'post_details/:postID',
+  //   builder: (BuildContext context, GoRouterState state) {
+  //     final extraData = state.extra as Map<String, dynamic>?;
 
-      return PostDetail(
-        postID: state.pathParameters['postID']!,
-        avatarURL: extraData?['avatarUrl'],
-        post: extraData?['postItem'],
-        username: extraData?['username'],
-      );
-    },
-  ),
+  //     return PostDetail(
+  //       postID: state.pathParameters['postID']!,
+  //       avatarURL: extraData?['avatarUrl'],
+  //       post: extraData?['postItem'],
+  //       username: extraData?['username'],
+  //     );
+  //   },
+  // ),
+  // GoRoute(
+  //   path: 'create_post',
+  //   builder: (BuildContext context, GoRouterState state) => const CreatePost(),
+  // ),
+  // GoRoute(
+  //   path: 'post_details/:postID/add_comment',
+  //   builder: (BuildContext context, GoRouterState state) {
+  //     return AddCommentPage(postId: state.pathParameters['postID']!);
+  //   },
+  // ),
   GoRoute(
-    path: 'create_post',
-    builder: (BuildContext context, GoRouterState state) => const CreatePost(),
-  ),
-  GoRoute(
-      path: 'post_details/:postID/add_comment',
-      builder: (BuildContext context, GoRouterState state) {
-        return AddCommentPage(postId: state.pathParameters['postID']!);
-      }),
-  GoRoute(
-    path: 'profile/:userID',
+    path: 'profile_page',
     builder: (BuildContext context, GoRouterState state) {
-      return ProfilePage(userID: state.pathParameters['userID']!);
+      final profile = state.extra as Profile;
+      return ProfilePage(profile: profile);
     },
   ),
   GoRoute(
     path: 'edit_profile',
     builder: (BuildContext context, GoRouterState state) {
-      return const EditProfilePage();
+      final profile = state.extra as Profile;
+      return  EditProfilePage(profile: profile);
     },
   ),
   GoRoute(
-    path: 'profile_settings',
+    path: 'profile_settings_page',
     builder: (BuildContext context, GoRouterState state) {
-      return const ProfileSettings();
+      final profile = state.extra as Profile;
+      return ProfileSettingsPage(profile: profile);
     },
   ),
-  GoRoute(
-    path: 'update_password',
-    builder: (BuildContext context, GoRouterState state) {
-      return const UpdatePassword();
-    },
-  ),
-  GoRoute(
-    path: 'explore',
-    builder: (BuildContext context, GoRouterState state) {
-      return const Search();
-    },
-  ),
+  // GoRoute(
+  //   path: 'update_password',
+  //   builder: (BuildContext context, GoRouterState state) {
+  //     return const UpdatePassword();
+  //   },
+  // ),
+  // GoRoute(
+  //   path: 'explore',
+  //   builder: (BuildContext context, GoRouterState state) {
+  //     return const Search();
+  //   },
+  // ),
 ];
